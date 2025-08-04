473e58fcd5bf8e30915fd428f1d9c8e63c49d144
neonrainsoftware <darikhovdev@proton.me>
2025-08-04 16:17:40 -0400

    problem 121

--- maths.cpp
+++ maths.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <math.h>
 
 class Solution_268 {
 public:
@@ -16,6 +17,25 @@ public:
         //using this number, we can find the difference between the sums to find which number is missing
         int idealSum = (nums.size() * (nums.size() + 1)) / 2;
         return idealSum - actualSum;
+    }
+};
+
+class Solution_121 {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        if (prices.size() == 0)
+            return 0;
+
+        //initialize variables, one set to the start, one set to our maximum profit
+        int minStock = prices[0];
+        int maxProf = 0;
+        for (int i = 0; i < prices.size(); i++ ) {
+            //get the max between the current max profit and the index value - the minimum value
+            maxProf = std::max(maxProf, prices[i] - minStock);
+            //reset the minimum value based either the index value or whatever the min stock was, whichever is lower
+            minStock = std::min(prices[i], minStock);
+        }
+        return maxProf;
     }
 };
 
